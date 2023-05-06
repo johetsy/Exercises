6c46c74c407f2021b5587944e2da7ff5f078ecb7
johetsy <johetsy@gmail.com>
2023-05-05 22:36:45 -0400

    exercise 4 and 5

---
+++ exercise4.py
@@ -0,0 +1,6 @@
+
+horas = int(input("Ingresa el numero de horas trabajadas: "))
+coste = float(input("Ingresa es coste por hora trabajada: "))
+paga = horas * coste
+
+print("Tu paga es de: ", paga, '$')
---
+++ exercise5.py
@@ -0,0 +1,7 @@
+
+cantidad = float(input("Ingresa la cantidad a invertir: "))
+interes = float(input("Ingresa el interés anual "))
+años = int(input("Ingresa el numero de años: "))
+capital = str(round(cantidad * (interes / 100 + 1) ** años, 2))
+
+print("Capital Obtenido: ", capital)
